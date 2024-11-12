4408beb012ba8872ccebd12c3298bc91b8faa706
vivekb77 <vivek2020bheke@gmail.com>
2024-11-12 16:11:25 +1300

    referrerPolicy origin on search api so mapbox wont reject

--- app/api/search-address/route.tsx
+++ app/api/search-address/route.tsx
@@ -1,21 +1,25 @@
 import { NextResponse } from "next/server";
-const BASE_URL="https://api.mapbox.com/search/searchbox/v1/suggest"
-export async function GET(request:any) {
+const BASE_URL = "https://api.mapbox.com/search/searchbox/v1/suggest"
+export async function GET(request: any) {
 
-    const {searchParams}=new URL(request.url);
+    const { searchParams } = new URL(request.url);
 
-    const searchText=searchParams.get('q');
+    const searchText = searchParams.get('q');
     const sessionToken = searchParams.get('session_token');
 
     const res = await fetch(
-        `${BASE_URL}?q=${searchText}&language=en&session_token=${sessionToken}&access_token=${process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN_SEARCH_RETRIEVE}`
+
+        `${BASE_URL}?q=${searchText}&language=en&session_token=${sessionToken}&access_token=${process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN_SEARCH_RETRIEVE}`,
+        {
+            referrerPolicy: 'origin',  // or 'origin-when-cross-origin'
+        }
     );
 
-    console.log("Search session token - " +sessionToken + "access token - " + process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN_SEARCH_RETRIEVE);
+    console.log("Search session token - " + sessionToken + "access token - " + process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN_SEARCH_RETRIEVE);
 
-    
 
-    const searchResult=await res.json();
+
+    const searchResult = await res.json();
     return NextResponse.json(searchResult)
-    
+
 }
