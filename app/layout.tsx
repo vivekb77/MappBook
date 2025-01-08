1ed3f5e19e8f2d2763ef5a60215717e1e45368d4
vivekb77 <vivek2020bheke@gmail.com>
2025-01-08 15:23:38 +1300

    added tiktok pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,44 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full">
       <head>
+      <Script id="tiktok-pixel" strategy="afterInteractive">
+          {`
+            !function (w, d, t) {
+              w.TiktokAnalyticsObject=t;
+              var ttq=w[t]=w[t]||[];
+              ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"];
+              ttq.setAndDefer=function(t,e){
+                t[e]=function(){
+                  t.push([e].concat(Array.prototype.slice.call(arguments,0)))
+                }
+              };
+              for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);
+              ttq.instance=function(t){
+                for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);
+                return e
+              };
+              ttq.load=function(e,n){
+                var r="https://analytics.tiktok.com/i18n/pixel/events.js";
+                var o=n&&n.partner;
+                ttq._i=ttq._i||{};
+                ttq._i[e]=[];
+                ttq._i[e]._u=r;
+                ttq._t=ttq._t||{};
+                ttq._t[e]=+new Date;
+                ttq._o=ttq._o||{};
+                ttq._o[e]=n||{};
+                var i=document.createElement("script");
+                i.type="text/javascript";
+                i.async=!0;
+                i.src=r+"?sdkid="+e+"&lib="+t;
+                var s=document.getElementsByTagName("script")[0];
+                s.parentNode.insertBefore(i,s);
+              };
+              ttq.load('CTUTUJJC77UBA10LS7B0');
+              ttq.page();
+            }(window, document, 'ttq');
+          `}
+        </Script>
         <Script id="reddit-pixel" strategy="afterInteractive">
           {`
             !function(w,d){if(!w.rdt){var p=w.rdt=function(){p.sendEvent?p.sendEvent.apply(p,arguments):p.callQueue.push(arguments)};p.callQueue=[];var t=d.createElement("script");t.src="https://www.redditstatic.com/ads/pixel.js",t.async=!0;var s=d.getElementsByTagName("script")[0];s.parentNode.insertBefore(t,s)}}(window,document);
