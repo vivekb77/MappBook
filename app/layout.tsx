6fd1a623ae27e8011a9ee7141e9b46456f6268ae
vivekb77 <vivek2020bheke@gmail.com>
2025-01-29 16:56:47 +1300

    added google ads pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,10 +8,21 @@ import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'MappBook.com',
-  description: 'Geotargeting Analytics for Amazon Seller Central Order Reports',
+  description: 'Location Intelligence for Amazon Sellers',
   robots: {
     index: true,
     follow: true,
+  },
+  // Add additional metadata for better SEO
+  openGraph: {
+    title: 'MappBook.com',
+    description: 'Location Intelligence for Amazon Sellers',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'MappBook.com',
+    description: 'Location Intelligence for Amazon Sellers',
   }
 };
 
@@ -21,7 +32,7 @@ export const viewport: Viewport = {
   maximumScale: 1,
   minimumScale: 1,
   userScalable: false,
-  viewportFit: 'cover'
+  viewportFit: 'cover',
 };
 
 export default function RootLayout({
@@ -32,42 +43,17 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full">
       <head>
-      <Script id="tiktok-pixel" strategy="afterInteractive">
+        {/* Google Tag Manager Script - Strategy: afterInteractive ensures it loads after page interaction */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-16818860554"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
           {`
-            !function (w, d, t) {
-              w.TiktokAnalyticsObject=t;
-              var ttq=w[t]=w[t]||[];
-              ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"];
-              ttq.setAndDefer=function(t,e){
-                t[e]=function(){
-                  t.push([e].concat(Array.prototype.slice.call(arguments,0)))
-                }
-              };
-              for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);
-              ttq.instance=function(t){
-                for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);
-                return e
-              };
-              ttq.load=function(e,n){
-                var r="https://analytics.tiktok.com/i18n/pixel/events.js";
-                var o=n&&n.partner;
-                ttq._i=ttq._i||{};
-                ttq._i[e]=[];
-                ttq._i[e]._u=r;
-                ttq._t=ttq._t||{};
-                ttq._t[e]=+new Date;
-                ttq._o=ttq._o||{};
-                ttq._o[e]=n||{};
-                var i=document.createElement("script");
-                i.type="text/javascript";
-                i.async=!0;
-                i.src=r+"?sdkid="+e+"&lib="+t;
-                var s=document.getElementsByTagName("script")[0];
-                s.parentNode.insertBefore(i,s);
-              };
-              ttq.load('CTUTUJJC77UBA10LS7B0');
-              ttq.page();
-            }(window, document, 'ttq');
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-16818860554');
           `}
         </Script>
         <Script id="reddit-pixel" strategy="afterInteractive">
